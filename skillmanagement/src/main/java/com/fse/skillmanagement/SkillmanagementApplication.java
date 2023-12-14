46ae9ede3b69a51f271614903917fbf2f6e695fe
Sagan484 <marc.nicolaus@web.de>
2023-12-14 11:39:06 +0100

    added project
    added project with two domains

---
+++ skillmanagement/src/main/java/com/fse/skillmanagement/SkillmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.fse.skillmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillmanagementApplication.class, args);
+	}
+
+}
